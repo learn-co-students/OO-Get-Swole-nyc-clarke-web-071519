5cf87e1c755670ee0aaf5aed6df82e814b7e2014
Valentino Wong <valentino.wong@gmail.com>
2019-07-19 00:37:33 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,36 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  #Get a list of all gyms
+  def self.all
+    @@all
+  end
+
+  #Get a list of all memberships at a specific gym
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  #Get a list of all the lifters that have a membership to a specific gym
+  def lifters
+    memberships.map {|membership| membership.lifter}
+  end
+
+  #Get a list of the names of all lifters that have a membership to that gym
+  def lifter_names
+    lifters.map {|lifter| lifter.name}
+  end
+
+  #Get the combined lift total of every lifter that has a membership to that gym
+  def lifters_lift_total
+    gym_lift_totals = lifters.map {|lifter| lifter.lift_total}
+    gym_lift_totals.reduce {|total, lift_total| total + lift_total}
+  end
+
 end
