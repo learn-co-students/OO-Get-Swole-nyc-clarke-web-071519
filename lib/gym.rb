dabc761f990f1c3ac7ecf184a8574751911751b4
Alyssa Farina <alyssafarina695@gmail.com>
2019-07-22 00:06:09 +0000

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -4,4 +4,22 @@ class Gym
   def initialize(name)
     @name = name
   end
+  
+  def memberships
+   Membership.all.select {|membership| membership.gym == self}
+  end
+  
+  def lifters
+    Membership.all.map { |membership| membership.lifter} 
+end
+
+  def lifter_names
+    lifters.map { |lifter| lifter.name}
+  end
+  
+  def lift_total
+    gym_lift_totals = lifters.map {|lifter| lifter.lift_total}
+    gym_lift_totals.reduce {|total, lift_total| total + lift_total}
+  end
+
 end
